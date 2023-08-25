5760b1b57e90cd6234e1f2ef44c541ed60b74b17
haewon <haewon1106@gmail.com>
2023-08-25 10:37:24 +0900

    4계절

--- 20230621_bookreviwecalender/js/review.js
+++ 20230621_bookreviwecalender/js/review.js
@@ -1,63 +1,127 @@
-// URL book에 해당하는 parameter 값 읽어오기(string)
+// // URL book에 해당하는 parameter 값 읽어오기(string)
+// let url = new URL(location.href);
+// let book = url.searchParams.get("book");
+// //book이 null이면 책장으로 가기
+// if(book === null){
+//     location.href = 'index.html';
+// }
+
+// console.log(`book: ${book}`);
+
+// // book값(string) -> bookId(number) 변환하기 : "3" -> 3
+// // let bookId = parseInt(book); //number
+// // let bookId = book * 1; //number (쓰지않기!)
+// let bookId = Number(book);
+// console.log(`bookId: ${bookId} type: ${typeof bookId}`); //number 3
+
+// // book 를 돌면서 bookId(number)와 같은 id값의 책을 가져오자
+// let bookData;
+// for(let bookOne of books){
+//     if(bookId === bookOne.id){  //=== : 값이랑 자료형이 모두 동일할 때
+//         bookData = bookOne;
+//         break;
+//     }
+
+// }
+// // 제목 보여주자
+// let title = bookData.title;
+// console.log(title);
+// // console.log(bookData["title"]);
+
+
+// let author = bookData.author;
+// let publisher = bookData.publisher;
+// let bookImage = bookData.bookImage;
+// console.log(title, author, publisher, bookImage);
+
+// // HTML 요소 -> js 변수
+// // js 변수.innerHTML
+
+// const titleDiv = document.getElementsByClassName("title")[1];
+// titleDiv.innerHTML = title;
+
+// const authorDiv = document.getElementsByClassName("author")[1];
+// authorDiv.innerHTML = author;
+
+// const publisherDiv = document.getElementsByClassName("publisher")[1];
+// publisherDiv.innerHTML = publisher;
+
+// const bookImageDiv = document.getElementsByClassName("book-image")[0];
+// bookImageDiv.innerHTML = `<img src="${bookImage}"/>`;
+
+// // 읽은 날짜 : 임시로 오늘의 날짜 표시
+// let now = new Date();
+// // 오늘의 날짜 구하기
+// let year = now.getFullYear();
+// let month = now.getMonth() + 1;
+// let date = now.getDate();
+
+// // 사람이 알아보는 형식으로 바꾸기
+// let dateString = `${year}년 ${month}월 ${date}일`;
+// // HTML -> js
+// const readDataDiv = document.getElementsByClassName("read-date")[1];
+// //오늘의 날짜 값 넣기
+
+
+//URL book에 해당하는 parameter 값 읽어오자(string)
 let url = new URL(location.href);
 let book = url.searchParams.get("book");
-//book이 null이면 책장으로 가기
-if(book === null){
-    location.href = 'index.html';
+console.log(`book: ${book} type: ${typeof book}`);      //"3" string
+//book이 null이면 책장으로 가자
+if (book === null) {
+    // window.open("index.html", "_top");
+    location.href = "index.html";
 }
 
-console.log(`book: ${book}`);
+//book값(string) -> bookId(number) 변환하자 "3" -> 3
+//let bookId = book * 1;    //절대 쓰지마
+//let bookId = Number(book);  //string -> number
+let bookId = parseInt(book);    //string -> number
+console.log(`bookId: ${bookId} type: ${typeof bookId}`);      //3 number
 
-// book값(string) -> bookId(number) 변환하기 : "3" -> 3
-// let bookId = parseInt(book); //number
-// let bookId = book * 1; //number (쓰지않기!)
-let bookId = Number(book);
-console.log(`bookId: ${bookId} type: ${typeof bookId}`); //number 3
-
-// book 를 돌면서 bookId(number)와 같은 id값의 책을 가져오자
+//books 를 돌면서 bookId(number)와 같은 id값의 책을 가져오자
 let bookData;
-for(let bookOne of books){
-    if(bookId === bookOne.id){  //=== : 값이랑 자료형이 모두 동일할 때
+for (let bookOne of books) {
+    if (bookId === bookOne.id) {    //===: 값이랑 자료형이랑 둘다 맞을 때 ㅇㅈ
         bookData = bookOne;
         break;
     }
-
 }
-// 제목 보여주자
+
+//제목 보여주자
 let title = bookData.title;
 console.log(title);
 // console.log(bookData["title"]);
-
-
 let author = bookData.author;
 let publisher = bookData.publisher;
-let bookImage = bookData.bookImage;
+let bookImage = bookData.img;
 console.log(title, author, publisher, bookImage);
 
-// HTML 요소 -> js 변수
-// js 변수.innerHTML
+const bookImageDiv = document.getElementsByClassName("book-image")[0];
+bookImageDiv.innerHTML = `<img src="${bookImage}" />`;
 
+//HTML 요소 -> js 변수
 const titleDiv = document.getElementsByClassName("title")[1];
+// const titleDiv = document.querySelectorAll(".title");
+//js 변수.innerHTML
 titleDiv.innerHTML = title;
+titleDiv.setAttribute("title", title);
 
 const authorDiv = document.getElementsByClassName("author")[1];
 authorDiv.innerHTML = author;
-
 const publisherDiv = document.getElementsByClassName("publisher")[1];
 publisherDiv.innerHTML = publisher;
 
-const bookImageDiv = document.getElementsByClassName("book-img")[0];
-bookImageDiv.innerHTML = `<img src="${bookImage}"/>`;
-
-// 읽은 날짜 : 임시로 오늘의 날짜 표시
+//읽은 날짜: 임시로 오늘의 날짜 표시하자
 let now = new Date();
-// 오늘의 날짜 구하기
+//오늘의 날짜 구하자
 let year = now.getFullYear();
 let month = now.getMonth() + 1;
 let date = now.getDate();
 
-// 사람이 알아보는 형식으로 바꾸기
+//사람이 알아보는 형식으로 바꾸자
 let dateString = `${year}년 ${month}월 ${date}일`;
-// HTML -> js
-const readDataDiv = document.getElementsByClassName("read-date")[1];
-//오늘의 날짜 값 넣기
+//HTML -> js
+const readDateDiv = document.getElementsByClassName("read-date")[1];
+//오늘의 날짜 값 넣자
+readDateDiv.innerHTML = dateString;
